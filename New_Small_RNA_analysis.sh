3021abc91a479941c221ab11f00d320e79dbb794
Linhua Sun <jshaslh@163.com>
2016-07-18 11:21:47 -0400

    add small RNA analysis pipeline from bam files

---
+++ New_Small_RNA_analysis.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+New_ShortStack --outdir test_nohp_140 --nohp --bamfile mismatch_1_BN1_140_R1_clean_sorted.bam  --genomefile Athaliana_167.fa &
+New_ShortStack --outdir test_nohp_141 --nohp --bamfile mismatch_1_BN1_141_R1_clean_sorted.bam  --genomefile Athaliana_167.fa &
+New_ShortStack --outdir test_nohp_142 --nohp --bamfile mismatch_1_BN1_142_R1_clean_sorted.bam  --genomefile Athaliana_167.fa &
+New_ShortStack --outdir test_nohp_143 --nohp --bamfile mismatch_1_BN1_143_R1_clean_sorted.bam  --genomefile Athaliana_167.fa &
